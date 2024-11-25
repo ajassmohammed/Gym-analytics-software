12ab706ab63461f3215952c0b5c135257f22c493
Ajas S Mohammed <84236291+ajassmohammed@users.noreply.github.com>
2024-11-25 16:12:10 +0530

    Setting Up the Project.js

--- Setting Up the Project.js
+++ Setting Up the Project.js
@@ -1,4 +1,4 @@
-mkdir gym-analytics-backend
-cd gym-analytics-backend
+ mkdir gym-analytics-backend
+ cd gym-analytics-backend
  npm init -y
  npm install express mongoose bcryptjs jsonwebtoken cors dotenv body-parser
